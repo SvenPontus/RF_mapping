7a660eec0d0bb58b29c271c392c2d1c6b54000be
Pontus Simonsson <ponsi25@lnx00334.ad.liu.se>
2024-11-25 15:39:47 +0100

    24-11-25

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import deeplabcut
 from dlchelperclass import DlcHelperClass as dhc 
 
 # For running many projects
-
+"""
 paths = [
-    "dlcrnet_stride32_ms5_batchsize_8-conv_vid-2024-11-21"
+    "one_vid_hrnet48-liu-2024-11-25",
+    "one_vid_res50-liu-2024-11-25",
+    "one_vid_stride32-liu-2024-11-25"
+
 ]
 
 
@@ -28,7 +31,11 @@ for path in paths:
     dhc.plot_loss_to_png(project_path)
 
 
-"""
+    
+
+
+
+    
 project_path = "/local/data2/LIA_LIU_PONTUS/LIA_LIU/hrnet_w48_2-conv_vid-2024-11-18"
 
 config_path, video_path = dhc.get_config_and_video_paths(project_path=project_path)
@@ -44,3 +51,38 @@ deeplabcut.create_labeled_video(config_path, videos=video_path)
 dhc.save_mean_likelihood_to_file(project_path=project_path)
 dhc.plot_loss_to_png(project_path)
 """
+
+
+import os
+import glob
+
+# Predict several videos and models
+
+paths = [
+    "one_vid_hrnet48-liu-2024-11-25",
+    "one_vid_res50-liu-2024-11-25",
+    "one_vid_stride32-liu-2024-11-25"
+
+]
+base_path = "/local/data2/LIA_LIU_PONTUS/LIA_LIU/"
+
+for path in paths:
+    project_path = base_path + path.strip()  
+    
+    config_path, video_path = dhc.get_config_and_video_paths(project_path=project_path)
+
+    video_path = "/local/data2/LIA_LIU_PONTUS/LIA_LIU/untrained_videos/Crop_predict_24_11_25/"
+
+    # Assuming your videos have extensions like .mp4, .avi, etc.
+    video_extensions = ('*.mp4', '*.avi', '*.mkv')  # Add other extensions if needed
+    video_files = []
+
+    for ext in video_extensions:
+        video_files.extend(glob.glob(os.path.join(video_path, ext)))
+
+    for video_file in video_files:
+        
+        deeplabcut.analyze_videos(config_path, video_file)
+        
+        deeplabcut.create_labeled_video(config_path, videos=video_file)
+
